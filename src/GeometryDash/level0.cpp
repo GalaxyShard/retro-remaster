5268269ffcb2f983ef984effb55754dbb83ed11e
GalaxyShard <swift.galaxyshard@gmail.com>
2022-04-01 01:07:54 -0400

    changes

--- src/GeometryDash/level0.cpp
+++ src/GeometryDash/level0.cpp
@@ -8,11 +8,10 @@ struct DashLevel0
     AssetRef<Mesh> square;
 
     std::unique_ptr<AudioPlayer> bgAudio;
-    std::unique_ptr<Material> playerMat;
+    std::unique_ptr<Material> playerMat, otherMat;
 
-
-    float gravity = -9.81f;
-    float speed = 1;
+    float gravity = -25.9f;
+    float speed = 10;
     float jumpHeight = 2;
     Object *player;
     Vector3 vel;
@@ -24,6 +23,17 @@ struct DashLevel0
     DashLevel0();
     ~DashLevel0();
 };
+
+bool test_aabb(Vector2 aabbMin0, Vector2 aabbMax0, Vector2 aabbMin1, Vector2 aabbMax1)
+{
+    return !(((aabbMin1.x >= aabbMax0.x) || (aabbMin0.x >= aabbMax1.x))
+        && ((aabbMin1.y >= aabbMax0.y) || (aabbMin0.y >= aabbMax1.y)));
+}
+bool test_obb()
+{
+    return 0;
+}
+
 void DashLevel0::handle_touch(TouchData data)
 {
     jump = data.state == TouchState::PRESSED;
@@ -38,20 +48,22 @@ void DashLevel0::pre_render()
     player->position.x += speed * Time::delta();
     player->position.y += vel.y * Time::delta();
     vel.y += gravity * Time::delta();
-    if (player->position.y <= -1)
+    if (player->position.y <= -1+player->scale.x/2)
     {
-        player->position.y = -1;
+        player->position.y = -1+player->scale.x/2;
         vel.y = -0.1f;
+        player->rotation.z = 0;
     }
     else 
     {
-        player->rotation.z += Math::PI*1.5f*Time::delta();
+        player->rotation.z -= Math::PI*2.f*Time::delta();
     }
-    Camera::main->position.x = (player->position.x + 5.f) * Renderer::reverseAspect.x;
+    Camera::main->position.x = player->position.x + 7.5f;
 }
 DashLevel0::DashLevel0()
 {
-    Camera::main->projection = Matrix4x4::ortho(-10.f, 10.f, -10.f, 10.f, -10.f, 100000.f, 0);
+    Camera::main->orthoSize = 10;
+    Camera::main->refresh();
     menuTex = Texture::load(Assets::path()+"/textures/menuButton.png", Texture::Pixel);
 
     renderConn = Renderer::pre_render().connect(CLASS_LAMBDA(pre_render));
@@ -82,12 +94,24 @@ DashLevel0::DashLevel0()
     player->scale = Vector2(1, 1);
     player->position = Vector2(0, -0.75);
 
+    otherMat = std::make_unique<Material>(colShader.get());
+    otherMat->set_uniform("u_color", Vector4(0.5,0.5,0.9,1));
+    Object *other = Object::create(square.get(), otherMat.get());
+    other->scale = Vector2(10000, 1);
+    other->position = Vector2(0, -1.5);
+
+    //for (uintG i = 0; i < 1000; ++i)
+    //{
+    //    Object *other = Object::create(square.get(), otherMat.get());
+    //    other->scale = Vector2(0.1, 0.1);
+    //    other->position = Vector2(rand()/(float)RAND_MAX*100, rand()/(float)RAND_MAX * 11 - 1);
+    //}
+
     Input::add_bind("jump", Key::Space, [this](bool p){jump=p;});
 }
 DashLevel0::~DashLevel0()
 {
     Input::remove_bind("jump");
-    Camera::main->position = Vector2(0,0);
-    Camera::main->projection = Matrix4x4::ortho(-1, 1, -1, 1, -100000, 100000, 0);
+    Camera::main->reset();
 }
 ADD_SCENE_COMPONENT("Dash-Level0", DashLevel0);
